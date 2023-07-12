1ff0e5fabaac10e321e523f4c5db7b6cbb15336c
Chinmaya Kaundanya <kaundanyachinmaya07@gmail.com>
2023-07-12 10:00:50 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cellSegmentation',
+    version='0.0.0',
+    author='Chinmaya Kaundanya',
+    author_email='kaundanyachinmaya07@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
